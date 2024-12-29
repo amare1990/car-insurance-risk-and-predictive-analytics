c9c7f1deb7dcf12f01d3978a2f1daf84cf52d206
amare1990 <amaremek@gmail.com>
2024-12-29 16:13:16 +0300

    Create ABHypothesisTesting class initialized with the dataset

---
+++ scripts/hypothesis_testing.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+import pandas as pd
+
+
+class ABHypothesisTesting:
+    def __init__(self, data: pd.DataFrame):
+        """
+        Initialize the class with the dataset.
+        :param data: A pandas DataFrame containing the dataset.
+        """
+        self.data = data
