6f3b47956218fe770053a3a54f43c127c474e742
amare1990 <amaremek@gmail.com>
2024-12-31 10:37:23 +0300

    Add a class instance method to split data into train and test data

--- scripts/statistical_modeling.py
+++ scripts/statistical_modeling.py
@@ -5,23 +5,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, accuracy_score, precision_score, recall_score, f1_score
-import xgboost as xgb
+# import xgboost as xgb
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
+
+from scripts.data_cleaning import DataCleaning
+
 class StatisticalModeling:
-    def __init__(self, data: pd.DataFrame):
+    def __init__(self):
         """
         Initialize the class with the dataset.
         :param data: A pandas DataFrame containing the dataset.
         """
+        input_file="../data/MachineLearningRating_v3.csv"
+        output_file="../data/final_cleaned_data_stmodeling.csv"
+        data_cleaner = DataCleaning(input_file, output_file)
+        data = data_cleaner.process_pipeline()
+
         self.data = data
         self.X_train = None
         self.X_test = None
         self.y_train = None
         self.y_test = None
         self.models = {}
+
 
     def handle_missing_data(self, threshold=0.3):
         """
@@ -64,5 +73,19 @@ class StatisticalModeling:
         else:
             raise ValueError(f"Unsupported encoding method: {method}")
         print(f"Encoded categorical columns using {method} encoding.")
+
+    def split_data(self, target="TotalClaims", test_size=0.2):
+        """
+        Split the dataset into training and testing sets.
+        :param target: The column to predict (usually 'TotalClaims' or 'TotalPremium').
+        :param test_size: The proportion of the data to use for testing.
+        """
+        X = self.data.drop(columns=[target])
+        y = self.data[target]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X, y, test_size=test_size, random_state=42
+        )
+        print(f"Data split into {len(self.X_train)} train samples and
+              {len(self.X_test)} test samples.")
 
 
